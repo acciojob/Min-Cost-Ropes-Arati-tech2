b53bd494dd4dc617db80429d63224f9a9ef3a39e
Arati-tech2 <aratiwak2@gmail.com>
2026-02-25 00:03:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let totalCost = 0; 
+	while(arr.length > 1){
+		arr.sort((a,b) => a-b);
+
+		let first = arr.shift();
+		let second = arr.shift();
+		let cost = first + second;
+	 totalCost += cost;
+
+		arr.push(cost);
+	}
+	return totalCost;
 }
 
 module.exports=mincost;
